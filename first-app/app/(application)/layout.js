030796a72485d7b20e34aa5ff7b09bb8723251fd
Shivamprajapati844505 <sp1172389@gmail.com>
2025-07-14 04:12:41 +0530

    3 section completed

--- first-app/app/(application)/layout.js
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({ children }) {
-  return (
-    <>
-      <header style={{ background: "pink" }}>Header Application</header>
-      {children}
-      <footer style={{ background: "green" }}>Footer Application</footer>
-    </>
-  );
-}
--- first-app/app/_components/page.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ComponentPage() {
-  return (
-    <div>ComponentPage</div>
-  )
-}
